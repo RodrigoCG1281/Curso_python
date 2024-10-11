8d2f3de3692fd34d2456356afd0cf4edb3985637
Rodrigo <rodri153060@gmail.com>
2024-10-10 20:34:19 -0500

    Agregando metodos de las candenas

--- clase4.py
+++ clase4.py
@@ -1,4 +1,22 @@
+#Usar comillas simples o dobles para las cadenas de texto, elegir uno
 name = "Hola Rodrigo Bernardo"
-apellido = "Condori Gutierrez"
+last_name = "    Condori                     GuTierrez"
 
-print(name + " " + apellido)
+#concatenacion
+print(name + " " + last_name)
+
+#replicaicon
+print(name * 3)
+
+#cantidad de caracteres
+print(len(name))
+
+#cantidad de caracteres
+print(len(last_name))
+
+#Metodos de cadenas
+print(name.lower()) #minusculas
+
+print(name.upper()) #mayusculas
+
+print(last_name.strip()) #Elimina espacios en blanco
