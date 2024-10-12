e147c19a8ff4d782fc32645868722137131fcc07
Rodrigo <rodri153060@gmail.com>
2024-10-11 23:22:24 -0500

    Operaciones matematicas

---
+++ clase6.py
@@ -0,0 +1,32 @@
+#Operaciones matematicas en python
+
+a = 15
+b = 10
+print("Suma:",a + b) #Suma
+print("Resta:",a - b) #Resta
+print("Multiplicacion:",a * b) #Multiplicacion
+print("Division:",a / b) #Division
+print("Division exacta:",a // b) #Division exacta
+print("Modulo:",a % b) #Modulo
+print("Potencia:",a ** b) #Potencia
+print("Raiz cuadrada:",a ** 0.5) #Raiz cuadrada
+
+"""
+P -> Paréntesis
+E -> Exponenciación
+M -> Multiplicación
+D -> División
+A -> Adición
+S -> Sustracción
+"""
+operation1 = 3 + 4 * 5 #23
+operation2 = (3 + 4) * 5 #35
+
+print("Operation 1:",operation1)
+print("Operation 2:",operation2)
+
+
+#Valores booleanos
+print("True and True:",True and True) #True
+print("4 > 5:",4 > 5) #False
+print("4 < 5:",4 < 5) #True
